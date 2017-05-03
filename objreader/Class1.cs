27a96537487c16df360cd07eb5b33370a514ab4a
Daniel Walsh <dangwalsh@gmail.com>
2017-05-03 11:42:21 -0400

    Add project files.

---
+++ objreader/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objreader
+{
+    public class Class1
+    {
+    }
+}
